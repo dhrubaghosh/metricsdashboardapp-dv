d3bcaa0494ffb68731dfbd5b55b8e7eb937c8540
dhrubaghosh <dhruba.ghosh2015@gmail.com>
2025-08-25 23:12:39 +0530

    Landing page view updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="bg-tcsBlue shadow text-center py-4 mt-8 rounded-lg mt-32">
+    <footer className="bg-tcsBlue shadow text-center py-4 mt-8 rounded-lg ">
       <span className="text-white text-sm">
         © 2025 All rights reserved.
       </span>
